3ac0259b0dee68f63b60080a4f6897dd0fc32b61
Ethan Tai <152665898+ethanstai@users.noreply.github.com>
2024-01-21 23:41:24 -0800

    Edit index file to add appropiate html elements for JS additions. Add dom manipulation to JS

--- script.js
+++ script.js
@@ -17,35 +17,74 @@ function getComputerChoice() {
     return compChoice;
 }
 
+/*
 function getUserChoice() {
     var selection = (prompt("Please enter your move; Rock, Paper, or Scissor: "));
     answer = selection.toLowerCase();
     return answer;
 }
+*/
+
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', () => {
+    singleGame('rock', getComputerChoice()); 
+});
+
+paper.addEventListener('click', () => {
+    singleGame('paper', getComputerChoice()); 
+});
+
+scissors.addEventListener('click', () => {
+    singleGame('scissors', getComputerChoice()); 
+});
+
+let gameDiv = document.querySelector('#single');
+let totalDiv = document.querySelector('#total');
 
 function singleGame(playerSelection, compSelection) {
-    var compSelection = getComputerChoice();
-    var playerSelection = getUserChoice();
-
-    console.log("Your move is " + playerSelection);
-    console.log("The computer chooses " + compSelection);
+    gameDiv.textContent = "";
+    compSelection = getComputerChoice();
+    //let playerSelection = getUserChoice();
 
     if (playerSelection === compSelection) {
-        console.log("It's a tie!");
+        gameDiv.textContent = "It's a tie!";
     } 
     else if (
         (playerSelection === "rock" && compSelection === "scissors") ||
         (playerSelection === "paper" && compSelection === "rock") ||
         (playerSelection === "scissors" && compSelection === "paper")
     ) {
-        console.log("You Win! " + playerSelection + " beats " + compSelection);
+        gameDiv.textContent = "You Win! " + playerSelection + " beats " + compSelection;
         playerScore++;
     } 
     else {
-        console.log("You Lose! " + compSelection + " beats " + playerSelection);
+        gameDiv.textContent = "You Lose! " + compSelection + " beats " + playerSelection;
         compScore++;
     }
+    
+    if (playerScore == 5) {
+        totalDiv.textContent = `You win the best out of 5!
+                                You: ${playerScore}
+                                Computer: ${compScore}`;
+    }
+    else if (compScore == 5) {
+        totalDiv.textContent = `Computer wins the best out of 5!    
+                                You: ${playerScore}
+                                Computer: ${compScore}`;
+                                //line breaks aren't registered with textContent because it doesn't interpret 
+    }
+    else {
+        totalDiv.textContent = "You: " + playerScore + " Computer: " + compScore;
+    }
+    
 }
+
+
+
+/*
 
 function game() {
     for (var i = 0; i < 5; i++) {
@@ -62,3 +101,4 @@ function game() {
 
 game();
 
+*/
